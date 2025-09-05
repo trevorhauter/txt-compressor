f5a651f8eaea9af501be05d6362f7fe533a8ea3c
Trevor <trevor@hauter.dev>
2025-09-04 20:19:10 -0400

    Formatting

--- src/Compressor.java
+++ src/Compressor.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.Scanner;
 
 
-
 public class Compressor {
     private static Map<String, Integer> buildCompressionMap() {
         Map<String, Integer> initialMap = new HashMap<String, Integer>();
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
